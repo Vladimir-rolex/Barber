296dda6f51c7b4491ac4c9423de74838a20ae2d7
Vladimir-rolex <jaroshko.vova@gmail.com>
2017-03-30 22:13:08 +0200

    BARBERSHOP

---
+++ js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	$(".main-nav-icon").click(function() {
+	    $(".main-nav ul").slideToggle( "slow", function() {
+    // Animation complete.
+       });
+	});   
+});
